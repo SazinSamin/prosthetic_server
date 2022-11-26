b8067990fd095a1a4a38e490c964a8a995a1ad28
SazinSamin <sazinsamin50@gmail.com>
2022-11-27 00:28:29 +0600

    add prescription to send email system

--- email/emailRoutehandler.js
+++ email/emailRoutehandler.js
@@ -3,14 +3,39 @@ import emailService from "./emailService.js";
 
 const emailRoute = express.Router();
 
-
 emailRoute.post('/', async(req, res) => {
-	console.log(req.body);
+        
         // handle post request for email service
-//        emailService.sendEmail("Test again from the server side", (err) => {
-//                err ? res.send(err) : res.send("Message succesfully send to the patient");
-//        });
-	res.send("Data successfully got from the frontend");
+        const [prescription, email] = prescriptionMsg(req.body);
+
+        emailService.sendEmail("OK", email,  (err) => {
+                err ? res.send(err) : res.send("Message has successfully delivered to the patient");
+        });
+        // res.end();
 });
+
+const prescriptionMsg = (data) => {
+        let msg = `Prescription: ${data['prescription']}
+
+
+Issue: ${data['Date']}`;
+        
+        msg += `
+
+Professor Dr. Megboron Paul
+Degree: MBBS, MS (Ortho), FICS, FACS (USA), DNB(ortho), Fellow in Pediatric Orthopedics
+Designation: Head, Orthopedic Department
+Expertise: Orthopaedic
+Org: Ibrahim Memorial Medical College & Hospital
+Chamber: Dhaka Central Hospital
+Location: Block-A, Sayednagar, Vatara,Dhaka, Bangladesh
+Phone: +88 01728346234`
+
+        const email = data['email'];
+
+        return [msg, email];
+}
+
+
 
 export default emailRoute;
--- email/emailService.js
+++ email/emailService.js
@@ -1,32 +1,38 @@
 import nodemailer from 'nodemailer';
 
 const emailService = {};
+
 emailService.transporter = nodemailer.createTransport({
         service: 'outlook',
         auth: {
-                user: 'sazinsamin@outlook.com',
+                user: "prosthetic_healthcare@outlook.com",
                 pass: process.env.emailPass,
         }
 });
 
-emailService.setConfig = (msg) => {
+emailService.setConfig = (msg, email) => {
         const config = {
-                from: 'sazinsamin@outlook.com',
-                to: 'sazinsamin50@gmail.com',
-                subject: 'Smart Healthcare',
+                from: "prosthetic_healthcare@outlook.com",
+                to: email ? email: "sazinsamin50@gmail.com",
+                subject: "Smart Healthcare of Prosthetic",
                 text: msg,
-        };
+        }
         return config;
 };
 
-emailService.sendEmail = (msg, callback) => {
-        emailService.transporter.sendMail(emailService.setConfig(msg), (err, data) => {
+emailService.sendEmail = (msg, email, callback) => {
+        emailService.transporter.verify((err, success) => {
+                if(err) {
+                        console.log(err);
+                } else {
+                        console.log('Ready....');
+                }
+        });
+        emailService.transporter.sendMail(emailService.setConfig(msg, email), (err, data) => {
+                console.log(err);
                 err ? callback(err) : callback(null);
         });
-};
+        // callback(null);
+}
 
 export default emailService;
-
-// emailService.sendEmail("Test from the server", (err) => {
-//         err ? console.log(err) : console.log("Email send successfully");
-// });
